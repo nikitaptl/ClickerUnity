ac7a312f5bda6efce522e04b33427b5f443a6236
Potylitsin Nikita <nspotylitsin@edu.hse.ru>
2024-03-24 02:35:53 +0300

    feat(code): New resource system Enum with resources, implementation of ObservableInt and ResourceBank was added

---
+++ Assets/_Source/Core/ObservableInt.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _Source.Core
+{
+    public class ObservableInt
+    {
+        private int _value;
+        public Action<int> OnValueChanged;
+
+        public ObservableInt()
+        {
+            _value = 0;
+        }
+
+        public ObservableInt(int value)
+        {
+            _value = value;
+        }
+        
+        public int Value
+        {
+            get => _value;
+            set
+            {
+                OnValueChanged?.Invoke(value);
+                _value = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return _value.ToString();
+        }
+    }
+}
